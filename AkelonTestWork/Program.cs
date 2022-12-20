5b67fb2f458f0e737d234318c58a365220c2a0b8
Kuro0001 <antaa200@gmail.com>
2022-12-20 13:27:51 +0300

    Добавьте файлы проекта.

---
+++ AkelonTestWork/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkelonTestWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SumCount work = new SumCount();
+            Console.WriteLine(work.Distribute(10000, "1000;2000;3000;5000;8000;5000", DistributionType.proportional));  //Выход: 416.67;833.33;1250;2083.33;3333.33;2083.34
+            Console.WriteLine(work.Distribute(10000, "1000;2000;3000;5000;8000;5000", DistributionType.first_count));  //Выход: 1000;2000;3000;4000;0;0
+            Console.WriteLine(work.Distribute(10000, "1000;2000;3000;5000;8000;5000", DistributionType.last_count));  //Выход: 0;0;0;0;5000;5000
+            Console.ReadKey();
+            /*
+            Console.WriteLine("tests for proportional method");
+            Console.WriteLine(work.Distribute(10000, "3333;3333;3333", DistributionType.proportional));  //Выход: 3333,33;3333,33;3333,33
+            Console.WriteLine(work.Distribute(10000, "3333,33;3333;3333", DistributionType.proportional));  //Выход: 3333,55;3333,22;3333,22
+            Console.WriteLine(work.Distribute(10000, "1;1;1", DistributionType.proportional));  //Выход: 3333,33;3333,33;3333,33
+            Console.WriteLine(work.Distribute(10000, "0;0;1", DistributionType.proportional));  //Выход: 0;0;10000
+            Console.ReadKey();
+            Console.WriteLine("tests for count method");
+            Console.WriteLine(work.Distribute(10000, "1000;1000;1000;1000;1000;1000", DistributionType.first_count));  //Выход: 1000;1000;1000;1000;1000;1000
+            Console.WriteLine(work.Distribute(10000, "10000;1000;1000;1000;1000;1000", DistributionType.first_count));  //Выход: 10000;0;0;0;0;0
+            Console.WriteLine(work.Distribute(10000, "100000;1000;1000;1000;1000;1000", DistributionType.first_count));  //Выход: 10000;0;0;0;0;0
+            Console.WriteLine(work.Distribute(10000, "0;0;0;0;0;0", DistributionType.first_count));  //Выход: 0;0;0;0;0;0
+            Console.ReadKey();
+            */
+        }
+    }
+}
---
+++ AkelonTestWork/SumCount.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkelonTestWork
+{
+    /// <summary>
+    /// Перечисление типов распределения сумм
+    /// <br/>
+    /// proportional - (ПРОП) через пропорции
+    /// <br/>
+    /// first_count - (ПЕРВ) на всю сумму по порядку
+    /// <br/>
+    /// last_count - (ПОСЛ) на всю сумму в обратном порядке
+    /// </summary>
+    public enum DistributionType
+    {
+        proportional,
+        first_count,
+        last_count
+    }
+    /// <summary>
+    /// класс для работы над
+    /// </summary>
+    class SumCount
+    {
+        public string Distribute(double sum, string sums, DistributionType type)
+        {
+            double[] distribution_sums = Array.ConvertAll<string, double>(sums.Split(';'), Double.Parse);
+            double[] result_sums = new double[distribution_sums.Count()];
+            switch (type)
+            {
+                case DistributionType.proportional:
+                    double[] sums_weight = new double[distribution_sums.Count()];
+                    for (int i=0; i < sums_weight.Count(); i++)
+                    {
+                        sums_weight[i] = distribution_sums[i] / distribution_sums.Sum();
+                        result_sums[i] = Math.Round(sum * sums_weight[i], 2);
+                    }
+                    if (result_sums.Sum() > sum)
+                        result_sums[result_sums.Count() - 1] = result_sums[result_sums.Count() - 1] - (result_sums[result_sums.Count() - 1] - sum);
+                    break;
+
+                case DistributionType.first_count:
+                    result_sums = DistributeLikeCount(sum, distribution_sums);
+                    break;
+
+                case DistributionType.last_count:
+                    Array.Reverse(distribution_sums);
+                    result_sums = DistributeLikeCount(sum, distribution_sums);
+                    Array.Reverse(result_sums);
+                    break;
+
+                default:
+
+                    break;
+            }
+            return String.Join(";", result_sums);
+        }
+
+        private double[] DistributeLikeCount(double sum, double[] distribution_sums)
+        {
+            double[] result = new double[distribution_sums.Count()];
+            int i = 0;
+            while ((sum > 0) && (i < distribution_sums.Count()))
+            {
+                if (sum - distribution_sums[i] > 0)
+                    result[i] = distribution_sums[i];
+                else
+                    result[i] = sum;
+                sum -= result[i];
+                i++;
+            }
+            return result;
+        }
+    }
+}
